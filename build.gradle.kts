7869f404a5fcb574d38e46cbef4fc47993f9561b
honkling <untitledgeese@gmail.com>
2024-08-30 17:43:05 -0500

    multiproject

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+	`maven-publish`
+}
+
+group = "org.skriptlang"
+version = "2.9.1"
+
+repositories {
+	mavenCentral()
+}
+
+subprojects {
+	apply(plugin = "java")
+	apply(plugin = "maven-publish")
+
+	publishing {
+		publications {
+			create<MavenPublication>("maven") {
+				groupId = "org.skriptlang"
+				artifactId = project.name
+				version = rootProject.version.toString()
+
+				from(components["java"])
+			}
+		}
+	}
+}
